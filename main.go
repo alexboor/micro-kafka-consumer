1a72eaebaab48548210de201179c053f93e105a3
aleksandr.stolbov <aleksandr.stolbov@libertexgroup.com>
2020-08-05 00:34:13 +0200

    (refactor): consume all partitions

--- main.go
+++ main.go
@@ -3,88 +3,110 @@ package main
 import (
 	"fmt"
 	"github.com/Shopify/sarama"
+	"log"
+	"os"
+	"os/signal"
+	"strings"
 )
 
 const (
 	CLIENTID = "micro-kafka-consumer"
-	BROKERS = "127.0.01:29092"
-	TOPIC = "in-example"
+	BROKERS  = "127.0.0.1:29092"
+	TOPIC    = "in-example"
 )
 
 func main() {
 
+	master := initConsumer()
 
+	consumer, errors := consume([]string{TOPIC}, master)
 
-	c := initConsumer(2)
+	signals := make(chan os.Signal, 1)
+	signal.Notify(signals, os.Interrupt)
 
-	consume(TOPIC, c)
+	doneChan := make(chan struct{})
 
+	go func() {
+		for {
+			select {
+			case msg := <-consumer:
+				processMsg(msg)
+			case consumerError := <-errors:
+				fmt.Println("recieve consume error", string(consumerError.Topic), string(consumerError.Partition), consumerError.Err)
+				doneChan <- struct{}{}
+			case <-signals:
+				fmt.Println("User interrupt detected")
+				doneChan <- struct{}{}
+			}
+		}
+	}()
 
-
+	<-doneChan
+	fmt.Println("Bye!")
 
 }
 
-func initConsumer (o int64) sarama.PartitionConsumer {
-	var startOffset int64
+func initConsumer() sarama.Consumer {
+	sarama.Logger = log.New(os.Stdout, "", log.Ltime)
 
 	config := sarama.NewConfig()
 	config.ClientID = CLIENTID
 	config.Consumer.Return.Errors = true
 
-	client, err := sarama.NewClient([]string{BROKERS}, config)
+	brokers := []string{BROKERS}
+
+	master, err := sarama.NewConsumer(brokers, config)
 	if err != nil {
-		fmt.Println("error create client: ", err)
+		fmt.Println("error create master consumer: ")
+		panic(err)
 	}
 
-	master, err := sarama.NewConsumerFromClient(client)
-	if err != nil {
-		fmt.Println("error create consumer: ", err)
-	}
 	defer func() {
 		if err := master.Close(); err != nil {
-			fmt.Println(err)
+			panic(err)
 		}
 	}()
 
-	partitions, err := master.Partitions(TOPIC)
-
-	offset, err := client.GetOffset(TOPIC, partitions[3], -1)
-	if err != nil {
-		fmt.Println("error get offset: ", err)
-	}
-
-	if o < 0 {
-		startOffset = offset + o
-	} else {
-		startOffset = sarama.OffsetNewest
-	}
-
-	fmt.Println("Avaliable partitions: ", partitions)
-	fmt.Println("Offset: ", offset, "Starting from: ", startOffset)
-
-	consumer, err := master.ConsumePartition(TOPIC, partitions[3],startOffset)
-	if err != nil {
-		fmt.Printf("Topic %v Partitions: %v", TOPIC, partitions)
-		fmt.Println("error consume messages")
-	}
-
-	return consumer
+	return master
 }
 
-func consume(t string, c sarama.PartitionConsumer) {
-	fmt.Println("Start consuming topic: ", TOPIC)
+func consume(topics []string, master sarama.Consumer) (chan *sarama.ConsumerMessage, chan *sarama.ConsumerError) {
+	consumers := make(chan *sarama.ConsumerMessage)
+	errors := make(chan *sarama.ConsumerError)
 
-	go func(t string, c sarama.PartitionConsumer) {
-		for {
-			select {
-			case consumerError := <- c.Errors():
-				fmt.Println("consumer error: ", consumerError)
-
-			case msg := <- c.Messages():
-				processMsg(msg)
-			}
+	for _, t := range topics {
+		if strings.Contains(t, "__consumer_offsets") {
+			continue
 		}
-	}(t, c)
+
+		partitions, _ := master.Partitions(t)
+
+		for k, _ := range partitions {
+
+			c, err := master.ConsumePartition(t, partitions[k], sarama.OffsetOldest)
+			if err != nil {
+				fmt.Printf("Topic: %d Partition: %d", t, partitions[k])
+				fmt.Println(err)
+			}
+			fmt.Println("consuming messages...")
+
+			go func(t string, c sarama.PartitionConsumer) {
+				for {
+					select {
+					case consumerError := <-c.Errors():
+						errors <- consumerError
+						fmt.Println("consume error: ", err)
+					case msg := <-c.Messages():
+						consumers <- msg
+						processMsg(msg)
+					}
+				}
+			}(t, c)
+
+		}
+	}
+
+	return consumers, errors
 }
 
 func processMsg(msg *sarama.ConsumerMessage) {
